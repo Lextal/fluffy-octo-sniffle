f1e2ca519e1d03a5512abec6109f738cd5823552
Roman Trusov <gimmepython@gmail.com>
2018-01-19 20:34:13 +0300

    iou scores

--- layers/losses.py
+++ layers/losses.py
@@ -2,6 +2,25 @@ import torch
 from torch import nn
 
 
+def iou_scores(logits, y, n_class=19):
+    """
+    Computes unweighted IoU scores for multiclass segmentation
+
+    :param logits: Variable or FloatTensor, assumed to be (N, C, H, W)
+    :param y: Variable or LongTensor, assumed to be (N, H, W)
+    :param n_class: number of classes
+    """
+    out = torch.max(logits, 1)[1]  # argmax
+    res = []
+    for cls in range(n_class):
+        _p = (out == cls)
+        _t = (y == cls)
+        tp = (_p * _t).sum()
+        union = ((_p + _t) > 0).sum()
+        res.append(-1 if union == 0 else tp * 100 / union)
+    return res
+
+
 def dice(input, target):
     smooth = 1e-9
 
@@ -24,3 +43,4 @@ class BCEWithDiceLoss(nn.Module):
 
     def forward(self, out, y):
         return self.bce(out, y) + self.alpha * dice(out, y)
+

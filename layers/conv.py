cfd3072e0cb80134c2d82aa8544feb271b2e9115
Roman Trusov <r.v.trusov@gmail.com>
2018-01-24 11:40:43 +0300

    more generic multi-dilation conv

--- layers/conv.py
+++ layers/conv.py
@@ -2,22 +2,24 @@ import torch
 from torch import nn
 
 
-class ParallelDilated(nn.Module):
-    def __init__(self, _in: int, _out: int, dilations: tuple, kernel_size=3, bn=False):
-        super(ParallelDilated, self).__init__()
+class MultiDilationConv2d(nn.Module):
+    def __init__(self, _in: int, _out: int, dilations: tuple, kernel_size=3, stride=1, bn=False, activation=nn.ReLU):
+        super().__init__()
         assert len(dilations) != 0
-        self.convs = nn.ModuleList([self.__make_conv_block__(_in, kernel_size, d, bn) for d in dilations])
-        self.final = nn.Conv2d(len(dilations) * _in, _out, kernel_size=1, bias=False)
+        self.convs = nn.ModuleList([self.__padded_conv__(_in, _out, kernel_size, stride, d, bn, activation) for d in dilations])
+        self.final = nn.Conv2d(len(dilations) * _out, _out, kernel_size=1)
 
     @staticmethod
-    def __make_conv_block__(_in, kernel_size, dilation, bn=False):
+    def __padded_conv__(_in: int, _out: int, kernel_size:int, stride=1, dilation=1, bn=False, activation=None):
         padding = (kernel_size // 2) * dilation
-        mod = [nn.Conv2d(_in, _in,
-                         kernel_size=kernel_size, stride=1, dilation=dilation, padding=padding, bias=False)]
+        layers = []
+        conv = nn.Conv2d(_in, _out, kernel_size=kernel_size, stride=stride, dilation=dilation, padding=padding, bias=False)
+        layers.append(conv)
         if bn:
-            mod.append(nn.BatchNorm2d(_in))
-        mod.append(nn.ReLU())
-        return nn.Sequential(*mod)
+            layers.append(nn.BatchNorm2d(_out))
+        if activation is not None:
+            layers.append(activation())
+        return layers[0] if len(layers) == 1 else nn.Sequential(*layers)
 
     def forward(self, x):
         feats = []
